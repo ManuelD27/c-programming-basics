213378ffeec0f6161dd0861b971c7c92406bc1ba
Your Name <you@example.com>
2024-08-05 17:46:00 +0100

    new file:   Varibales/AssignVariable.c 	new file:   Varibales/simple.c

---
+++ .vscode/hello.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main()
+{
+    printf("Hello world");
+    printf("\nWelcome to C programming");
+    printf("\nHere you will learn the basics of C language");
+    printf("\nThis will be good practice for future progrmming!!!");
+
+    return 0;
+}
---
+++ Varibales/simple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 30;
+
+    printf("Current age: %d", age);
+
+    age = 31;
+
+    printf("\nNext year age: %d", age);
+
+    return 0;
+}
